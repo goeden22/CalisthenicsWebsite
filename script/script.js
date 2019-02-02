31726952b8db25f480f416d8628b3cf6e6bf9749
unknown <vooriden@gmail.com>
2019-02-02 21:55:46 +0100

    Making it full responsive

--- script/script.js
+++ script/script.js
@@ -1,16 +1,46 @@
-const gallery = document.getElementById('galleryContainer');
-const gallerytiles = document.querySelectorAll('.gallery__tile')
-
 function Gallery(id,arrows,counter) {
     this.container = document.getElementById(id);
     this.arrows = document.getElementById(arrows);
     this.counter = document.getElementById(counter);
-    this.currentImg = 0
-    this.dots = function(){
-        return this.counter.querySelectorAll('.gallery__dot')
+    this.currentImg = 0;
+    this.mode;
+    this.dots = ""
+    
+    this.dotsActive = function(dot, remove){
+        let dots = this.counter.querySelectorAll('.gallery__dot')  
+            dots[remove || 0].classList.remove('dot--active') 
+        dots[dot].classList.add('dot--active')
     }
     this.tiles = function () {
         return this.container.querySelectorAll('.gallery__tile')
+    }
+    this.setDots = function(){
+        this.dots = ""
+     for (let i = 0; i < this.tiles().length - this.mode; i++){
+         this.dots += '<div class="gallery__dot"></div>'
+     }   
+    }
+    this.attachDots = function(){
+        this.setDots();
+        this.counter.innerHTML = this.dots
+        this.dotsActive(this.currentImg);
+    }
+    this.setMode = function() {
+        let large = window.matchMedia('(min-width: 1306px)')
+        let medium = window.matchMedia('(max-width: 1306px) and (min-width: 885px)')
+        let small = window.matchMedia('(max-width: 884px)')
+
+        if(large.matches && this.mode != 2){
+            this.mode = 2;
+            this.attachDots()
+        } else if (medium.matches && this.mode != 1){
+            this.mode = 1;
+            this.attachDots()
+        }
+        else if (small.matches && this.mode != 0){
+            this.mode = 0;
+            this.attachDots()
+        }
     }
     this.singleWidth = function(){
         return $(gal.tiles()[0]).outerWidth(true);
@@ -25,15 +55,18 @@ function Gallery(id,arrows,counter) {
         if(this.currentImg < 0){
             this.currentImg = 0;
             return undefined
-        } else if (this.currentImg > (this.tiles().length - 3)){
-            this.currentImg = this.tiles().length - 3
+        } else if (this.currentImg > (this.tiles().length - this.mode - 1)){
+            this.currentImg = this.tiles().length - this.mode - 1
             return undefined
         }
         this.container.style.transform = `translateX(${-(this.currentImg * this.singleWidth())}px)`
-        this.dots()[this.currentImg - multiplier].classList.remove('dot--active')
-        this.dots()[this.currentImg].classList.add('dot--active')
+        this.dotsActive(this.currentImg, (this.currentImg - multiplier))
     },
     this.arrows.onclick = this.slide.bind(this)
+    document.body.onresize = this.setMode.bind(this)
+    this.setMode();
+    this.dotsActive(this.currentImg);
+    //wywołujemy metode przy tworzeniu każdej nowej galerii
 }
 
 let gal = new Gallery('galleryContainer', 'galleryArrows','galleryCounter')
